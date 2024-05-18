4ae9957c167bb1c1d563c36ece10290e2b68e3d0
vandewa <devandewananta@gmail.com>
2024-05-18 10:38:49 +0700

    tambah dashboard

--- app/Livewire/Dashboard.php
+++ app/Livewire/Dashboard.php
@@ -2,9 +2,11 @@
 
 namespace App\Livewire;
 
+use App\Models\Gaji;
+use App\Models\User;
+use Livewire\Component;
 use App\Models\Kendaraan;
 use App\Models\Pemeliharaan;
-use Livewire\Component;
 
 class Dashboard extends Component
 {
@@ -13,9 +15,17 @@ class Dashboard extends Component
     public $endDate;
     public $data;
 
+    public $laki, $perempuan, $total, $total_gaji;
+
     public function mount()
     {
-        // 
+        $this->laki = User::where('jk', 'l')->count();
+        $this->perempuan = User::where('jk', 'p')->count();
+        $this->total = User::whereHas('jabatan')->where('status', '1')->count();
+
+        $jml_diterima_gapok = Gaji::sum('jml_diterima_gapok');
+        $jml_diterima_tunjangan = Gaji::sum('jml_diterima_tunjangan');
+        $this->total_gaji = $jml_diterima_gapok + $jml_diterima_tunjangan;
     }
 
     public function updated($property)
--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,4 +1,82 @@
 <div>
-    {{-- <livewire:chart.kendaraan-chart>
-        <livewire:chart.kegiatan-chart> --}}
+    <div class="container">
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <!-- Info boxes -->
+                <div class="row">
+                    <div class="col-12 col-sm-6 col-md-3">
+                        <div class="info-box">
+                            <span class="info-box-icon bg-info elevation-1"><i class="fas fa-users"></i></span>
+
+                            <div class="info-box-content">
+                                <span class="info-box-text">Total Pegawai</span>
+                                <span class="info-box-number">
+                                    {{ $total }}
+                                </span>
+                            </div>
+                            <!-- /.info-box-content -->
+                        </div>
+                        <!-- /.info-box -->
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-12 col-sm-6 col-md-3">
+                        <div class="info-box mb-3">
+                            <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-male"></i></span>
+
+                            <div class="info-box-content">
+                                <span class="info-box-text">Laki-laki</span>
+                                <span class="info-box-number">{{ $laki }}</span>
+                            </div>
+                            <!-- /.info-box-content -->
+                        </div>
+                        <!-- /.info-box -->
+                    </div>
+                    <!-- /.col -->
+
+                    <!-- fix for small devices only -->
+                    <div class="clearfix hidden-md-up"></div>
+
+                    <div class="col-12 col-sm-6 col-md-3">
+                        <div class="info-box mb-3">
+                            <span class="info-box-icon bg-success elevation-1"><i class="fas fa-female"></i></span>
+
+                            <div class="info-box-content">
+                                <span class="info-box-text">Perempuan</span>
+                                <span class="info-box-number">{{ $perempuan }}</span>
+                            </div>
+                            <!-- /.info-box-content -->
+                        </div>
+                        <!-- /.info-box -->
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-12 col-sm-6 col-md-3">
+                        <div class="info-box mb-3">
+                            <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-dollar-sign"></i></span>
+
+                            <div class="info-box-content">
+                                <span class="info-box-text">Total Gaji</span>
+                                <span class="info-box-number">{{ \Laraindo\RupiahFormat::currency($total_gaji) }}</span>
+                            </div>
+                            <!-- /.info-box-content -->
+                        </div>
+                        <!-- /.info-box -->
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <!-- /.row -->
+                <div class="row mt-4">
+                    <div class="col-md-6">
+                        <livewire:chart.status-pekerjaan-chart>
+                    </div>
+                    <div class="col-md-6">
+                        <livewire:chart.tingkat-pekerjaan-chart>
+                    </div>
+                </div>
+
+            </div><!--/. container-fluid -->
+        </section>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
 </div>
